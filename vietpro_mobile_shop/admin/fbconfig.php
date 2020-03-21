1073b76cb3f5696e0a26ba249a4bf3652020f9b1
duydz6 <59145223+duydz6@users.noreply.github.com>
2020-03-21 14:27:46 +0700

    .
    Source code

---
+++ vietpro_mobile_shop/admin/fbconfig.php
@@ -0,0 +1,9 @@
+<?php
+
+$fb = new Facebook\Facebook([
+    'app_id' => '1733279970148176', // Thay thông tin app của bạn vào đây
+    'app_secret' => 'd8c5ee7ed88457fa039d3571d99be4ae',
+    'default_graph_version' => 'v6.0',
+        ]);
+?>
+
